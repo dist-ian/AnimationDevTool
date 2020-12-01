353d248edea3d8de043e02d1ebec8905885f581c
dist-ian <71764999+dist-ian@users.noreply.github.com>
2020-12-01 21:34:44 +0700

    Add files via upload

---
+++ Translation/values_in.lua
@@ -0,0 +1,97 @@
+-- only change the string after colon (:)
+
+local values_in = [[
+  {
+    "in": {
+      "draft_dummy_generator_00_title": "Alat Dev. Animasi",
+      "draft_dummy_generator_00_text": "Lampirkan animasi malam atau animasi lainnya ke pluginmu dengan mudah.",
+      "adt_copy": "Salin ke papan klip",
+      "adt_save": "Simpan menjadi file",
+      "adt_about": "Tentang",
+      "adt_set_text": "Atur",
+      "adt_enter_option": "Masukan",
+      "adt_add_option": "Tambahkan",
+      "adt_close_option": "Tutup",
+
+      "adt_generator_error_message": "Tolong hapus terlebih dahulu bangunan yang telah ada.",
+      "adt_select_dummy_title": "Pilih Ukuran Bangunan",
+
+      "adt_copy_totalLine_option": "Jumlah Baris",
+      "adt_copy_selectAll_option": "Pilih Semua",
+      "adt_copy_reset_option": "Reset",
+      "adt_copy_copy_option": "Salin",
+
+      "adt_about_plugin_version": "Versi",
+      "adt_about_plugin_author": "Pembuat",
+      "adt_about_translation_contrib": "Penerjemah",
+      "adt_about_manual": "Petunjuk",
+
+      "adt_direction_title": "Arah",
+      "adt_direction_manual": "Arah pengulangan animasi.",
+      "adt_column_title": "Kolom",
+      "adt_column_manual": "Pengulangan animasi secara horisontal.",
+      "adt_row_title": "Baris",
+      "adt_row_manual": "Pengulangan animasi secara vertikal.",
+      "adt_x_manual": "Titik awal animasi di koordinat X. Angka yang lebih besar, animasi bergerak ke kanan.",
+      "adt_y_manual": "Titik awal animasi di koordinat Y. Angka yang lebih besar, animasi bergerak ke bawah",
+      "adt_diffX_manual": "Jarak di antara kolom.",
+      "adt_diffY_manual": "Jarak di antara baris.",
+      "adt_offsetY_manual": "Perbedaan ketinggian di setiap kolom.",
+      "adt_probability_manual": "Peluang dari animasi yang akan ditampilkan. Jika probability bernilai 1, kode tidak akan ditulis.",
+      "adt_base_frame_manual": "Bingkai dari bangunan.\nPanel bingkai dasar bisa dipindahkan dengan cara mengklik tombol di sebelah tombol bingkai dasar, klik dan tahan baris judul, lalu seret ke posisi lain. Posisi akan selalu direset setiap permainan ditutup.",
+      "adt_animationId_title": "Id Animasi",
+      "adt_animationId_manual": "Animasi yang akan ditambahkan.",
+      "adt_layer_text": "Lapisan",
+      "adt_layer_manual": "Untuk mengatur banyak animasi dan tata letak yang berbeda.",
+
+      "adt_base_frame_title": "Bingkai Dasar",
+      "adt_base_type_title": "Tipe Bingkai",
+      "adt_base_type_frame_title": "Bingkai",
+      "adt_base_type_animation_title": "Animasi",
+      "adt_base_buildingId_title": "Id Bangunan",
+      "adt_base_buildingId_button": "Masukan Id Bangunan",
+      "adt_base_frame_index_title": "Indeks Bingkai",
+      "adt_base_layer_title": "Lapisan Dasar",
+      "adt_base_animationId_title": "Id Anim. Bangunan",
+      "adt_base_animationId_button": "Masukan Id Anim. Bangunan",
+      "adt_base_enter_x_title": "Masukan Nilai X",
+      "adt_base_enter_y_title": "Masukan Nilai Y",
+      
+      "adt_animationId_dialog_title": "Pilih Id Animasi",
+      "adt_animationId_new_title": "Tambahkan Id Animasi",
+      "adt_animationId_new_desc": "Masukan Id Animasi",
+
+      "adt_horizontal_title": "Horisontal",
+      "adt_vertical_title": "Vertikal",
+      "adt_rect_title": "Persegi",
+      "adt_diagonal_title": "Diagonal",
+
+      "adt_dialog_column_title": "Atur Kolom",
+      "adt_dialog_x_title": "Atur X",
+      "adt_dialog_diffX_title": "Atur Diff X",
+      "adt_dialog_probability_title": "Atur Probability",
+      "adt_dialog_probability_desc": "Masukan angka untuk probability lampu yang akan dinyalakan. Jika probability bernilai 1, maka kode \"night light probability\" tidak akan ditambahkan",
+      "adt_dialog_row_title": "Atur Row",
+      "adt_dialog_y_title": "Atur Y",
+      "adt_dialog_diffY_title": "Atur Diff Y",
+      "adt_dialog_offsetY_title": "Atur Offset Y",
+
+      "adt_layer_dialog_title": "Pengaturan Lapisan",
+      "adt_layer_counter_title": "Penghitung",
+      "adt_layer_new_title": "Buat Lapisan Baru",
+      "adt_layer_show_all_title": "Tampilkan Semua Animasi.",
+
+      "adt_save_success_message": "Disimpan ke ../TheoTown/Animation Dev Tool/_anim.txt",
+      "adt_save_first_error_message": "Err: Tolong isi id bangunan",
+      "adt_save_second_error_message": "Err: Tolong isi id animasi bangunan",
+      "adt_copy_success_message": "Menyimpan kode ke papan klip",
+      "adt_copy_error_message": "Err: Tolong isi id animasi",
+
+      // You can enter your name in here and will appear in the plugin about dialog, but you have to contribute in this translation. Please use comma to separate the multiple names and don't erase the written names.
+      // e.g : "Another Name, Your Name, Another Name"
+      "adt_contributor_list": "ian`"
+    }
+  }
+]]
+
+return values_in
