20e653c6b91865c52891d68e7a051f69be8f4215
dist-ian <71764999+dist-ian@users.noreply.github.com>
2020-12-01 22:41:37 +0700

    Add files via upload

---
+++ Translation/values_pl.lua
@@ -0,0 +1,97 @@
+-- only change the string after colon (:)
+
+local values = [[
+  {
+    "pl": {
+      "draft_dummy_generator_00_title": "Animation Dev.Tool",
+      "draft_dummy_generator_00_text": "Attach a night lights or animations to your plugin easily.",
+      "adt_copy": "Copy to clipboard",
+      "adt_save": "Save to file",
+      "adt_about": "About",
+      "adt_set_text": "Set",
+      "adt_enter_option": "Enter",
+      "adt_add_option": "Add",
+      "adt_close_option": "Close",
+
+      "adt_generator_error_message": "Please remove the last building first.",
+      "adt_select_dummy_title": "Select Building Size",
+
+      "adt_copy_totalLine_option": "Total line",
+      "adt_copy_selectAll_option": "Select All",
+      "adt_copy_reset_option": "Reset",
+      "adt_copy_copy_option": "Copy",
+
+      "adt_about_plugin_version": "Version",
+      "adt_about_plugin_author": "Author",
+      "adt_about_translation_contrib": "Translator",
+      "adt_about_manual": "Manual",
+
+      "adt_direction_title": "Direction",
+      "adt_direction_manual": "The direction of animations repeatition.",
+      "adt_column_title": "Column",
+      "adt_column_manual": "Repeat the animations on horizontal direction.",
+      "adt_row_title": "Row",
+      "adt_row_manual": "Repeat the animations on vertical direction.",
+      "adt_x_manual": "Anchor of X coordinates of animations. Bigger number, the animation going to right.",
+      "adt_y_manual": "Anchor of Y coordinates of animations. Bigger number, the animation going to down.",
+      "adt_diffX_manual": "Space between columns.",
+      "adt_diffY_manual": "Space between rows.",
+      "adt_offsetY_manual": "Different height between columns.",
+      "adt_probability_manual": "Chance of the animation will be appears. If probability is 1, the code will not be written.",
+      "adt_base_frame_manual": "The frame of building.\nThe base frame panel is can be moved by click the button on next of the base frame button, tap and hold the title bar, then drag into another positions. Positions will be reset every game closed.",
+      "adt_animationId_title": "Animation Id",
+      "adt_animationId_manual": "The animation that's will be added.",
+      "adt_layer_text": "Layer",
+      "adt_layer_manual": "To handle many of different animations and layouts.",
+
+      "adt_base_frame_title": "Base Frame",
+      "adt_base_type_title": "Base Type",
+      "adt_base_type_frame_title": "Frame",
+      "adt_base_type_animation_title": "Animation",
+      "adt_base_buildingId_title": "Building Id",
+      "adt_base_buildingId_button": "Enter Building Id",
+      "adt_base_frame_index_title": "Frame Index",
+      "adt_base_layer_title": "Base Layer",
+      "adt_base_animationId_title": "Building Anim. Id",
+      "adt_base_animationId_button": "Enter Building Anim. Id",
+      "adt_base_enter_x_title": "Enter X",
+      "adt_base_enter_y_title": "Enter Y",
+      
+      "adt_animationId_dialog_title": "Select Animation Id",
+      "adt_animationId_new_title": "Add New Animation",
+      "adt_animationId_new_desc": "Please enter animation id",
+
+      "adt_horizontal_title": "Horizontal",
+      "adt_vertical_title": "Vertical",
+      "adt_rect_title": "Rect",
+      "adt_diagonal_title": "Diagonal",
+
+      "adt_dialog_column_title": "set Column",
+      "adt_dialog_x_title": "Set X",
+      "adt_dialog_diffX_title": "Set Diff X",
+      "adt_dialog_probability_title": "Set Probability",
+      "adt_dialog_probability_desc": "Enter number for probability the light to spawn. If probability is 1, \"night light probability\" code will not be included",
+      "adt_dialog_row_title": "Set Row",
+      "adt_dialog_y_title": "Set Y",
+      "adt_dialog_diffY_title": "Set Diff Y",
+      "adt_dialog_offsetY_title": "Set Offset Y",
+
+      "adt_layer_dialog_title": "Layer Settings",
+      "adt_layer_counter_title": "Counter",
+      "adt_layer_new_title": "Create New Layer",
+      "adt_layer_show_all_title": "Show All Animations.",
+
+      "adt_save_success_message": "Saved into ../TheoTown/Animation Dev Tool/_anim.txt",
+      "adt_save_first_error_message": "Err: Please enter the building id",
+      "adt_save_second_error_message": "Err: Please enter the building anim. id",
+      "adt_copy_success_message": "Put the code into clipboard",
+      "adt_copy_error_message": "Err: Please enter the animation id",
+
+      // You can enter your name in here and will appear in the plugin about dialog, but you have to contribute in this translation. Please use comma to separate the multiple names and don't erase the written names.
+      // e.g : "Another Name, Your Name, Another Name"
+      "adt_contributor_list": ""
+    }
+  }
+]]
+
+return values
